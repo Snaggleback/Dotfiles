eb1e5097135c4bc2644760759d4e8fb0f92c0469
Ítalo <lixoliteral@gmail.com>
2024-04-12 14:36:12 -0300

    Update battery script
    Atualizei o script de bateria, para que ficasse menor em tamanho, mais légivel e manuntenível ao longo do tempo. Exclui as repetições de código, agora deixando o script mais claro e obviamente corrigido bugs e aplicando alguns "upgrades"

--- .scripts/info/show-battery.sh
+++ .scripts/info/show-battery.sh
@@ -1,65 +1,47 @@
 #!/bin/bash
 
-# Icones
-icon_battery_full="$HOME/.icons/system/battery-full.png"
-icon_battery_charging_full="$HOME/.icons/system/battery-charging-full.png"
-# Icones de bateria descarregando
-icon_battery_1_bar="$HOME/.icons/system/battery-1-bar.png"
-icon_battery_2_bar="$HOME/.icons/system/battery-2-bar.png"
-icon_battery_3_bar="$HOME/.icons/system/battery-3-bar.png"
-icon_battery_4_bar="$HOME/.icons/system/battery-4-bar.png"
-# Icones de status carregado
-icon_battery_charging_1_bar="$HOME/.icons/system/battery-charging-1-bar.png"
-icon_battery_charging_2_bar="$HOME/.icons/system/battery-charging-2-bar.png"
-icon_battery_charging_3_bar="$HOME/.icons/system/battery-charging-3-bar.png"
-icon_battery_charging_4_bar="$HOME/.icons/system/battery-charging-4-bar.png"
-
 # Porcetagem da bateria
 battery_level=$(acpi -b | awk '{print $4}' | tr -d '%,')
-
 # Carregando ou descarregando
 battery_status=$(acpi -b | awk '{print $3}' | tr -d ',')
 
-if [ "${battery_level}" -ge 98 ] || [ "${battery_status}" == "Not" ]; then
-    notify-send -a "Informações sobre a bateria" -r 6666 -i "${icon_battery_full}" "Sua bateria está completamente carregada!"
-    exit 0
+# Icone da bateria se for carregando ou descarregando
+[ "${battery_status}" == "Charging" ] && icon_variable="-charging"
+# Icone da bateria dependendo da porcetagem da bateria
+if [ "${battery_level}" -le 20 ]; then
+    icon_variable_bar="1-bar"
+elif [ "${battery_level}" -le 40 ]; then
+    icon_variable_bar="2-bar"
+elif [ "${battery_level}" -le 60 ]; then
+    icon_variable_bar="3-bar"
+elif [ "${battery_level}" -le 90 ]; then
+    icon_variable_bar="4-bar"
+elif [ "${battery_level}" -le 100 ]; then
+    icon_variable_bar="full"
 fi
 
+# Icone definitivo da bateria
+icon_battery="$HOME/.icons/system/battery${icon_variable}-${icon_variable_bar}.png"
+
+# Altera o status da bateria caso ela seja maior que 98 ou o status for igual a "Not"
+[ "${battery_level}" -ge 98 ] || [ "${battery_status}" == "Not" ] && battery_status="Full"
+# Altera o status da bateria caso ela seja menor que 18 para "Empty"
+[ "${battery_level}" -le 18 ] && battery_status="Empty"
 
 case "${battery_status}" in
-    "Discharging")
-        # Caso esteja descarregando; Icone
-        battery_discharging_icon="${icon_battery_full}"
-
-        # Icones caso a bateria esteja abaixo de:
-        if [ "${battery_level}" -le 20 ]; then
-            battery_discharging_icon="${icon_battery_1_bar}"
-        elif [ "${battery_level}" -le 40 ]; then
-            battery_discharging_icon="${icon_battery_2_bar}"
-        elif [ "${battery_level}" -le 60 ]; then
-            battery_discharging_icon="${icon_battery_3_bar}"
-        elif [ "${battery_level}" -le 90 ]; then
-            battery_discharging_icon="${icon_battery_4_bar}"
-        fi
-        
-        notify-send -a "Informações sobre a bateria" -r 6666 -i "${battery_discharging_icon}" "Sua bateria está descarregando, ela está em ${battery_level}%"
+"Discharging")
+    # Mostra informaçãões sobre a bateria se estiver descarregando
+    notify-send -a "Informações sobre a bateria" -r 6666 -i "${icon_battery}" "Sua bateria está descarregando, ela está em ${battery_level}%"
     ;;
-    "Charging")
-        battery_charging_icon="${icon_battery_charging_full}"
-
-         # Icones caso a bateria esteja abaixo de:
-        if [ "${battery_level}" -le 20 ]; then
-            battery_charging_icon="${icon_battery_charging_1_bar}"
-        elif [ "${battery_level}" -le 40 ]; then
-            battery_charging_icon="${icon_battery_charging_2_bar}"
-        elif [ "${battery_level}" -le 60 ]; then
-            battery_charging_icon="${icon_battery_charging_3_bar}"
-        elif [ "${battery_level}" -le 80 ]; then
-            battery_charging_icon="${icon_battery_charging_4_bar}"
-        fi
-
-        notify-send -a "Informações sobre a bateria" -r 6666 -i "${battery_charging_icon}" "Sua bateria está carregando, ela está em ${battery_level}%"
+"Charging")
+    # Mostra informaçãões sobre a bateria se estiver carregando
+    notify-send -a "Informações sobre a bateria" -r 6666 -i "${icon_battery}" "Sua bateria está carregando, ela está em ${battery_level}%"
+    ;;
+"Full")
+    # Mostra informaçãões sobre a bateria se estiver completamente carregada
+    notify-send -a "Informações sobre a bateria" -r 6666 -i "${icon_battery}" "Sua bateria está completamente carregada!"
     ;;
 esac
 
-echo "${battery_status}"
+# Mostra o status da bateria no terminal como uma forma de "debug"
+echo "Battery \"debug\": Ícone: ${icon_battery}, Porcentagem: ${battery_level}, Status: ${battery_status}"
