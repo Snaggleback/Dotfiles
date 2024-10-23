db4a2e3f4c35de8d84ca64b5df6143f2d094eba7
Ítalo <lixoliteral@gmail.com>
2024-10-23 20:36:15 -0300

    Atualiza: Arquivos e Pastas atualizadas/melhoradas
    Os arquivos foram atualizadas para atender melhor às necessidades. Arquivos modificados: .config/GIMP/2.10/tool-options/gimp-airbrush-tool (tipo: arquivo, foi modificado),  .config/GIMP/2.10/tool-options/gimp-bucket-fill-tool (tipo: arquivo, foi modificado),  .config/GIMP/2.10/tool-options/gimp-clone-tool (tipo: arquivo, foi modificado),  .config/GIMP/2.10/tool-options/gimp-color-picker-tool (tipo: arquivo, foi modificado),  .config/GIMP/2.10/tool-options/gimp-convolve-tool (tipo: arquivo, foi modificado),  .config/GIMP/2.10/tool-options/gimp-crop-tool (tipo: arquivo, foi modificado),  .config/GIMP/2.10/tool-options/gimp-dodge-burn-tool (tipo: arquivo, foi modificado),  .config/GIMP/2.10/tool-options/gimp-eraser-tool (tipo: arquivo, foi modificado),  .config/GIMP/2.10/tool-options/gimp-foreground-select-tool (tipo: arquivo, foi modificado),  .config/GIMP/2.10/tool-options/gimp-gradient-tool (tipo: arquivo, foi modificado),  .config/GIMP/2.10/tool-options/gimp-heal-tool (tipo: arquivo, foi modificado),  .config/GIMP/2.10/tool-options/gimp-ink-tool (tipo: arquivo, foi modificado),  .config/GIMP/2.10/tool-options/gimp-mypaint-brush-tool (tipo: arquivo, foi modificado),  .config/GIMP/2.10/tool-options/gimp-operation-tool (tipo: arquivo, foi modificado),  .config/GIMP/2.10/tool-options/gimp-paintbrush-tool (tipo: arquivo, foi modificado),  .config/GIMP/2.10/tool-options/gimp-pencil-tool (tipo: arquivo, foi modificado),  .config/GIMP/2.10/tool-options/gimp-perspective-clone-tool (tipo: arquivo, foi modificado),  .config/GIMP/2.10/tool-options/gimp-smudge-tool (tipo: arquivo, foi modificado),  .config/GIMP/2.10/tool-options/gimp-text-tool (tipo: arquivo, foi modificado),  .config/GIMP/2.10/tool-options/gimp-vector-tool (tipo: arquivo, foi modificado),  .config/i3/config (tipo: arquivo, foi modificado),  .icons/system/barcode-reader.png (tipo: arquivo, foi adicionado),  .icons/system/svg/barcode-reader.svg (tipo: arquivo, foi adicionado),  .scripts/system/barcode-reader.sh (tipo: arquivo, foi adicionado),

---
+++ .scripts/system/barcode-reader.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Captura uma região da tela com maim
+maim -o -u -s /tmp/barcode-reader-img.png
+
+# Lê o QR Code da captura usando zbarimg
+qr_result=$(zbarimg --raw /tmp/barcode-reader-img.png 2>/dev/null)
+
+# Verifica se o QR Code foi lido corretamente
+if [ -n "$qr_result" ]; then
+    # Copia o conteúdo do QR Code para a área de transferência usando xclip
+    echo "$qr_result" | xclip -selection clipboard
+
+    # Notifica o usuário com uma ação
+    notify-send -a "Leitor de códigos de barras" -r 6666 -i "$HOME/.icons/system/barcode-reader.png" \
+        "Conteúdo do QR Code copiado para a área de transferência: $qr_result"
+else
+    notify-send -a "Leitor de códigos de barras" -r 6666 -i "$HOME/.icons/system/barcode-reader.png" "Nenhum código QR encontrado"
+fi
+
+# Limpa o arquivo temporário
+rm /tmp/barcode-reader-img.png
