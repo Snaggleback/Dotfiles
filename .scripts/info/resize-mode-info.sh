55856af063a38733b31ae867ffa0cf51ee417967
Ítalo <lixoliteral@gmail.com>
2025-02-23 11:13:49 -0300

    Atualiza: Arquivos e Pastas atualizadas/melhoradas
    Os arquivos foram atualizadas para atender melhor às necessidades. Arquivos modificados: .config/htop/htoprc (tipo: arquivo, foi modificado)  .config/picom/picom.conf (tipo: arquivo, foi modificado)  .scripts/info/resize-mode-info.sh (tipo: arquivo, foi modificado)

--- .scripts/info/resize-mode-info.sh
+++ .scripts/info/resize-mode-info.sh
@@ -2,7 +2,7 @@
 
 case "${1}" in
     "on")
-        notify-send -a "Informações sobre o modo de redimensionamento" -r 2222 -i "$HOME/.icons/system/settings.png" -u critical -t 0 "Você ativou o modo de redimensionamento"
+        notify-send -a "Informações sobre o modo de redimensionamento" -r 2222 -i "$HOME/.icons/system/settings.png" -u critical "Você ativou o modo de redimensionamento"
     ;;
     "off")
         notify-send -a "Informações sobre o modo de redimensionamento" -r 2222 -i "$HOME/.icons/system/settings.png" -u normal -t 1000 "Você desativou o modo de redimensionamento"
